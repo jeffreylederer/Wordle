59254d5f24674f562fe12e7699efb328a1fdd3ba
Jeffrey Lederer <jeffrey@winnlederer.com>
2022-02-25 17:06:54 -0500

    update 1

--- src/Components/BoardRow.js
+++ src/Components/BoardRow.js
@@ -12,7 +12,7 @@ class BoardRow extends React.Component {
   			 <div className="row">
 			 <GameTile letter ={this.props.line.length === 0?'': this.props.line.substr(0,1)}/>
 			 <GameTile letter ={this.props.line.length < 1 ?'': this.props.line.substr(1,1)}/>
-			 <GameTile letter ={this.props.line.length < 2?'': this.props.line.substr(2,1)}/>
+			 <GameTile letter ={this.props.line.length < 2 ?'': this.props.line.substr(2,1)}/>
 			 <GameTile letter ={this.props.line.length < 3 ?'': this.props.line.substr(3,1)}/>
 			 <GameTile letter ={this.props.line.length < 4 ?'': this.props.line.substr(4,1)}/>
 	    	</div>
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -8,14 +8,15 @@ class Game extends React.Component {
 	constructor(props) {
 			super(props);
 			this.state = {
-				lines: Array(6).fill("abcd"),
+				lines: Array(6).fill(""),
 				currentLineNo: 0
 		};
 	}
-
+	
+	
 	handleKey = e => {
 		let currentWord = this.state.lines[this.state.currentLineNo]
-		if(this.state.currentLineNo == 6)
+		if(this.state.currentLineNo == 5)
 			return;
 		if(e.key === 'Shift') {
 			return;
@@ -24,12 +25,11 @@ class Game extends React.Component {
 		switch(e.key) {
 			case 'Enter': // CR
 				if(currentWord.length == 5) {
-					this.state.lines[this.state.currentLineNo] = currentWord;
-					currentWord="";
-					let currentLineNo = this.state.currentLineNo+1;
+					const currentLineNo = this.state.currentLineNo+1;
 					this.setState({currentLineNo: currentLineNo});
+					return;
 				}
-				break;
+				return;
 			case 'Backspace': //bS
 				if(currentWord.length > 0) {
 					currentWord = currentWord.substr(0, currentWord.length-1);
@@ -43,10 +43,7 @@ class Game extends React.Component {
 				if(code >= 65 && code <= 90) { // upper case
 					code = code + 32;
 				}
-				else if(code >= 97 && code <= 122) {  // lower case
-									
-				}
-				else {
+				else if(!(code >= 97 && code <= 122)) {  // lower case
 					return;
 				}
 			    currentWord += String.fromCharCode(code);
@@ -55,12 +52,13 @@ class Game extends React.Component {
 		const lines = this.state.lines.slice();
 		lines[this.state.currentLineNo] = currentWord;
 		this.setState({lines: lines});
+		alert(currentWord);
 	}
 	
    
 	render() {
 		return (
-    		<div onKeyDown={(e)=>this.handleKey(e)} tabIndex="0">
+    		<div onKeyDown={(e)=>this.handleKey(e)} tabIndex="-1">
 			 <BoardContainer lines={this.state.lines}/>
 			 <br/> <br/>
 			 <Keyboard />
